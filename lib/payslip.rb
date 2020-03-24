654ae0ddecdb6196d783a25b78fd0748c80fd19f
potateros <ericngu97@gmail.com>
2020-03-24 23:38:01 +0800

    made total_tax in payslip accessible

--- lib/payslip.rb
+++ lib/payslip.rb
@@ -2,19 +2,20 @@ require_relative 'tax_brackets.rb'
 
 class Payslip
   attr_accessor :name, :salary
+  attr_reader :total_tax
 
   def initialize(name, salary)
     @name = name
     @salary = salary
+    tax_bracket = TaxBrackets.new(@salary)
+    @total_tax = tax_bracket.calculate_tax.floor
   end
 
   def printout
-    tax_bracket = TaxBrackets.new(@salary)
-    total_tax = tax_bracket.calculate_tax
-    p "Monthly Payslip for: #{@name}"
-    p "Gross Monthly Income: $#{@salary/12}"
-    p "Monthly Income Tax: $#{total_tax/12}"
-    p "Net Monthly Income: $#{(@salary - total_tax)/12}"
+    p "Annual Payslip for: #{@name}"
+    p "Gross Annual Income: $#{@salary}"
+    p "Annual Income Tax: $#{@total_tax}"
+    p "Net Annual Income: $#{@salary - @total_tax}"
     return
   end
 
